2fa076cca434e2d898600f94df872c1b34e32a34
AnhCuong <phiquoccuong2803@gmail.com>
2026-06-13 16:01:04 +0700

    done gd admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('User/index');
+})->name('user.index');
+
+Route::prefix('admin')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('admin/dashboard');
+    })->name('admin.dashboard');
+
 });
