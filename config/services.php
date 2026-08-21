83b354583db956f5661c855ecfc3f02f11fd1467
uyduc9786-star <uyduc9786@gmail.com>
2026-08-21 16:16:41 +0700

    config(shipping): dua cau hinh ghn ra bien moi truong

--- config/services.php
+++ config/services.php
@@ -36,9 +36,20 @@ return [
     ],
 
     'google' => [
-    'client_id' => env('GOOGLE_CLIENT_ID'),
-    'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-    'redirect' => env('GOOGLE_REDIRECT_URI'),
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+    ],
+
+    'ghn' => [
+        'base_url' => env('GHN_BASE_URL', 'https://online-gateway.ghn.vn/shiip/public-api/v2'),
+        'token' => env('GHN_API_TOKEN'),
+        'shop_id' => env('GHN_SHOP_ID'),
+        'store_district_id' => env('STORE_DISTRICT_ID'),
+        'service_type_id' => (int) env('GHN_SERVICE_TYPE_ID', 2),
+        'default_fee' => (int) env('GHN_DEFAULT_FEE', 30000),
+        'default_item_weight' => (int) env('GHN_DEFAULT_ITEM_WEIGHT', 500),
+        'quote_ttl_minutes' => (int) env('GHN_QUOTE_TTL_MINUTES', 15),
     ],
 
 ];
